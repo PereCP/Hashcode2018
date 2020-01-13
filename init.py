b8567199534feb83c3fb5f08b203c3d753f3fdf8
Víctor Vivancos Serrano <victorvivancos@10-192-46-22client.eduroam.upc.edu>
2020-01-13 12:37:18 +0100

    Delete function

--- init.py
+++ init.py
@@ -13,28 +13,27 @@ class Ride:
 def distance(point1, point2):
     return abs(point1.x-point2.x) + abs(point1.y-point2.y)
 
-def readfile(inname, outname):
-    # Read file
-    f = open(inname)
-    # first line
-    l1 = f.readline()
-    l1 = l1.replace('\n', '')
-    l1 = l1.split(' ')
-    r = int(l1[0])
-    c = int(l1[1])
-    f = int(l1[2])
-    n = int(l1[3])
-    b = int(l1[4])
-    t = int(l1[5])
-    rides = []
-    for x in range(n):
-        ln = f.readline()
-        ln = ln.replace('\n', '')
-        ln = ln.split(' ')
-        #row = x
-        #column = y
-        pointstart = Point(ln[0], ln[1])
-        pointfinish = Point(ln[2], ln[3])
-        ride = Ride(pointstart, pointfinish, )
 
-readfile("a_example.in", "out.txt")
+# Read file
+f = open("a_example.in")
+# first line
+l1 = f.readline()
+l1 = l1.replace('\n', '')
+l1 = l1.split(' ')
+r = int(l1[0])
+c = int(l1[1])
+f = int(l1[2])
+n = int(l1[3])
+b = int(l1[4])
+t = int(l1[5])
+rides = []
+for x in range(n):
+    ln = f.readline()
+    ln = ln.replace('\n', '')
+    ln = ln.split(' ')
+    # row = x
+    # column = y
+    pointstart = Point(ln[0], ln[1])
+    pointfinish = Point(ln[2], ln[3])
+    ride = Ride(pointstart, pointfinish, ln[4], ln[5])
+    rides.append(ride)
